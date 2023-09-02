14f95edd36c20be799c511c47422562ab46e5823
Anastasiia Vorobets <anastasiiavorobets03@gmail.com>
2023-09-02 10:54:14 +0300

    todo-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { FilterActivePipe } from './pipes/filter-active.pipe';
+import { MessageComponent } from './components/message/message.component';
+import { TodosPageComponent } from './pages/todos-page/todos-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FilterComponent } from './components/filter/filter.component';
+
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then(m => m.AboutModule)
+  },
+  { path: 'todos/:status', component: TodosPageComponent },
+  { path: '**', redirectTo: '/todos/all', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterActivePipe,
+    FilterComponent,
+    MessageComponent,
+    TodoComponent,
+    TodoFormComponent,
+    TodosPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
